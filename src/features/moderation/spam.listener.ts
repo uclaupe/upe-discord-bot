d3e26bce54f4cf8771764cc8ddd64308949645f6
Vincent Lin <vinlin24@outlook.com>
2026-07-12 18:28:31 -0700

    Also delete spam msg and broadcast ACK embeds to moderation channel

--- src/features/moderation/spam.listener.ts
+++ src/features/moderation/spam.listener.ts
@@ -1,23 +1,80 @@
-import { Events, Message, PermissionFlagsBits } from "discord.js";
+import {
+  bold,
+  channelMention,
+  Colors,
+  EmbedBuilder,
+  Events,
+  inlineCode,
+  Message,
+  PermissionFlagsBits,
+  userMention,
+} from "discord.js";
 
 import { DiscordEventListener } from "../../abc/listener.abc";
 import {
   ADVISOR_ROLE_ID,
   EMERITUS_ROLE_ID,
+  MODERATION_CHANNEL_ID,
   OFFICERS_ROLE_ID,
 } from "../../utils/snowflakes.utils";
+import channelsService from "../../services/channels.service";
 
 class SpamListener extends DiscordEventListener<Events.MessageCreate> {
   public override readonly event = Events.MessageCreate;
 
   public override async execute(message: Message<true>): Promise<void> {
-    if (this.isProhibitedAtEveryoneAttempt(message)) {
-      const author = message.member;
+    await this.handleProhibitedAtEveryoneAttempt(message);
+  }
+
+  private async handleProhibitedAtEveryoneAttempt(
+    message: Message<true>,
+  ): Promise<void> {
+    if (!this.isProhibitedAtEveryoneAttempt(message)) {
+      return;
+    }
+
+    const author = message.member;
+    if (!author) {
+      return;
+    }
+
+    const overviewEmbed = new EmbedBuilder()
+      .setTitle("Suspected Spammer")
+      .setDescription(
+        `${userMention(author.id)} tried to ${inlineCode("@everyone")} in ` +
+        `${channelMention(message.channelId)}.`,
+      )
+      .setColor(Colors.Red);
+
+    const echoEmbed = new EmbedBuilder()
+      .setTitle("Suspected Spam Message")
+      .setDescription(message.content)
+      .setFooter({ text: `Included ${message.attachments.size} attachments` })
+      .setColor(Colors.Red);
+
+    if (author.kickable) {
       const channelName = message.channel.name;
-      await author?.kick(
+      await author.kick(
         `Unauthorized @everyone attempt in #${channelName}, likely spam`,
       );
+      overviewEmbed
+        .setTitle("Kicked Suspected Spammer")
+        .setDescription(
+          `${overviewEmbed.data.description} They have been ${bold("kicked")}.`,
+        )
+        .setColor(Colors.Yellow);
     }
+
+    if (message.deletable) {
+      await message.delete();
+      echoEmbed
+        .setTitle("Deleted Suspected Spam Message")
+        .setColor(Colors.Yellow);
+    }
+
+    await channelsService.sendToChannel(MODERATION_CHANNEL_ID, {
+      embeds: [overviewEmbed, echoEmbed],
+    });
   }
 
   private isProhibitedAtEveryoneAttempt(message: Message<true>): boolean {
